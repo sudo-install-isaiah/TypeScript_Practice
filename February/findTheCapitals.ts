1696a7c918d4240576fa8a5efb00b5ac95e7d4a2
sudo-install-isaiah <isaiah.iu@hotmail.com>
2024-02-13 18:25:28 -0500

    New file with instructions

---
+++ February/findTheCapitals.ts
@@ -0,0 +1,9 @@
+// Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters (word) as its argument, and returns an ordered list containing the indices of all capital (uppercase) letters in the string
+
+// "CodEWaRs"-- > [0, 3, 4, 6];
+
+export function capitals(word: string): number[] {
+	// Write your code here
+
+	return [] as number[];
+}
