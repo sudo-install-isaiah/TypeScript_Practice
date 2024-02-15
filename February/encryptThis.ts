a3f3c63641838dc4498ee1c4d7b373065da82bdd
sudo-install-isaiah <isaiah.iu@hotmail.com>
2024-02-15 16:37:21 -0500

    Add file and instructions for encryptThis

---
+++ February/encryptThis.ts
@@ -0,0 +1,15 @@
+// Your message is a string containing space separated words.
+// You need to encrypt each word in the message using the following rules:
+// The first letter must be converted to its ASCII code.
+// The second letter must be switched with the last letter
+// Keepin' it simple: There are no special characters in the input.
+// Examples:
+// encryptThis "Hello" == "72olle"
+// encryptThis "good" == "103doo"
+// encryptThis "hello world" == "104olle 119drlo"
+
+
+export const encryptThis = (str: string): string => {
+	// Your code goes here
+	return str;
+};
