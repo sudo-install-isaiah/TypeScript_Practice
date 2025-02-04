23087f47d6ac726904978f66d77d113300b25137
sudo-install-isaiah <isaiah.iu@hotmail.com>
2025-02-04 17:39:13 -0500

    Add exercise grow.ts w/solution

---
+++ 2025/February/grow.ts
@@ -0,0 +1,8 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+export function grow(arr: number[]): number {
+	// your code here
+	return arr.reduce((curr, acc) => curr * acc);
+}
